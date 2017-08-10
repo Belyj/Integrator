7c5a59367a7b6444f4149648dc7f704a93a5be04
Belyj <Belyj_07@inbox.ru>
2017-08-10 10:48:47 +0400

    RemoveContactByName исправил

--- src/main/resources/sqlScripts/Procedures/ContactProcedure/removeContactByName.sql
+++ src/main/resources/sqlScripts/Procedures/ContactProcedure/removeContactByName.sql
@@ -6,10 +6,4 @@ COMMENT '	seek and remove contact
 			@param contact name'
 BEGIN 
     DELETE FROM handbook_schema.contact_table WHERE contact_name = contactName; 
-    DELETE FROM handbook_schema.link_table WHERE contact_id = (SELECT contact_id FROM handbook_schema.contact_table WHERE contact_name = contactName);
 END// 
-
-
-DROP procedure if exists removeContactByName;
-
-CALL removeContactByName('D');
