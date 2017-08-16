b4709bfda01fb8742bbe5962472f1f8a2e2b22f2
Belyj <Belyj_07@inbox.ru>
2017-08-17 03:24:27 +0400

    Добавлен скрипт на поиско соответствия user

---
+++ src/main/resources/sqlScripts/Procedures/getUserByName.sql
@@ -0,0 +1,11 @@
+DELIMITER // 
+CREATE PROCEDURE `getUserByName` (IN userName VARCHAR(255), userPass VARCHAR(256)) 
+LANGUAGE SQL 
+DETERMINISTIC 
+COMMENT 'searching user info
+		@param user name, user pass'
+BEGIN 
+    SELECT uid, uname FROM handbook_schema.user_table WHERE uname = userName AND pass = userPass;
+END//
+
+call getUserByName("RU", "RU");
